2f08130eb58605998522eee67d2a55243fbcddc2
AishSI <aishsi@gmail.com>
2023-11-12 16:56:09 +0700

    feat: add solution "mine-sweeper" task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const matrixSize = {
+    RowCount: matrix.length,
+    ColumnCount: matrix[0].length,
+  };
+  const matrixWeightCell = Array.from(
+    new Array(matrixSize.RowCount),
+    () => new Array(matrixSize.ColumnCount)
+  );
+
+  matrix.forEach((row, indexRow) => {
+    row.forEach((cell, indexColumn) => {
+      matrixWeightCell[indexRow][indexColumn] = calcNear(
+        indexRow,
+        indexColumn,
+        matrix,
+        matrixSize
+      );
+    });
+  });
+  return matrixWeightCell;
+}
+
+function calcNear(row, col, matrix, size) {
+  let res = 0;
+  for (let offsetRow = -1; offsetRow <= 1; offsetRow += 1) {
+    for (let offsetCol = -1; offsetCol <= 1; offsetCol += 1) {
+      if (
+        outBounds(offsetRow + row, offsetCol + col, size) ||
+        (offsetRow === 0 && offsetCol === 0)
+      )
+        continue;
+      let currCell = matrix[offsetRow + row][offsetCol + col];
+      res += currCell ? 1 : 0;
+    }
+  }
+  return res;
+}
+
+function outBounds(y, x, size) {
+  return y < 0 || x < 0 || y >= size.RowCount || x >= size.ColumnCount;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
